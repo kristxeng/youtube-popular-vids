010adf0ebf46fcf5e5ec3658a69438b65b6b81a9
Kris <kristxeng@gmail.com>
2020-09-12 23:20:35 +0800

    feat: card component

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { VidItem, VidSnippet, VidContentDetails } from 'src/app/core/models/youtube-response.interface';
+import { DataHandlerService } from 'src/app/core/services/data-handler.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+
+  @Input() data;
+
+  videoInfo: VidSnippet;
+  thumbnailsUrl: string;
+  contentDetails: VidContentDetails;
+  durationStr: string;
+
+  constructor(private readonly dataHandler: DataHandlerService) { }
+
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  private initData(): void {
+    this.videoInfo = this.data.snippet;
+    this.thumbnailsUrl = this.videoInfo.thumbnails.medium.url;
+    this.contentDetails = this.data.contentDetails;
+    this.durationStr = this.dataHandler.parseDuration(this.data.contentDetails.duration);
+  }
+
+}
