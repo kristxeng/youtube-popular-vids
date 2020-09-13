d287e0f2f2910c94ccdf848e3d4e3fce7a8ac29f
Kris <kristxeng@gmail.com>
2020-09-14 00:15:54 +0800

    fix: modify vid url

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,6 +1,16 @@
+import { VidItem } from './../core/models/youtube-response.interface';
+import { DataHandlerService } from './../core/services/data-handler.service';
+/**
+ * Video.js and Angular integration
+ * from https://docs.videojs.com/tutorial-angular.html
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 
 const VID_URL = 'https://bitdasha.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
+const VID_URL2 = 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
 
 @Component({
   selector: 'app-play',
@@ -11,10 +21,21 @@ export class PlayComponent implements OnInit {
 
   options
 
-  constructor() { }
+  vid: VidItem;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly dataHandler: DataHandlerService,
+  ) { }
 
   ngOnInit(): void {
     this.options = this.getVjsOptions();
+    this.route.queryParams.pipe(
+      map( params => params.id),
+      switchMap( id => this.dataHandler.vids$.pipe(
+          map(vids => vids.find(vid => vid.id === id))
+      ))
+    ).subscribe( vid => this.vid = vid);
   }
 
   getVjsOptions() {
@@ -23,13 +44,12 @@ export class PlayComponent implements OnInit {
       autoplay: true,
       controls: true,
       sources: [{
-        src: VID_URL,
+        src: VID_URL2,
         type: 'application/x-mpegURL',
-        withCredentials: true,
       }],
       Html5: {
         vhs: {
-          withCredentials: true,
+          withCredentials: false,
         }
       },
     });
