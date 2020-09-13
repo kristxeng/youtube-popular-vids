90846052f66f4e05683ddae491ca272948fab797
Kris <kristxeng@gmail.com>
2020-09-13 14:10:51 +0800

    feat: pagination with route params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  }, {
+    path: 'home',
     component: HomeComponent
   }, {
     path: 'collection',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { VidItem } from 'src/app/core/models/youtube-response.interface';
 import { Component, OnInit } from '@angular/core';
 import { GoogleYoutubeApiService } from '../core/services/google-youtube-api.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +12,24 @@ export class HomeComponent implements OnInit {
 
   data: Array<VidItem>;
 
-  page
+  pageNum: number;
 
   constructor(
     private readonly apiService: GoogleYoutubeApiService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.pageNum = params.page;
+    });
+
     this.apiService.getList().subscribe( res => this.data = res.items );
   }
 
+  onPageChange(pageNum): void {
+    this.pageNum = pageNum;
+    this.router.navigate(['.'], {queryParams: {page: pageNum}});
+  }
 }
